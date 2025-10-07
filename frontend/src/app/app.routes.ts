9c6db1824955ad2e29319190da2a2fbcb34479e9
Daniel Dzvonik <daniel.dzvonik@kosickaakademia.sk>
2025-10-07 12:33:50 +0200

    created first register

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/register', pathMatch: 'full' }, // root presmeruje na register
+  { path: 'register', component: RegisterComponent }
+];
