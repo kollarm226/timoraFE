820f0b80f9be26ea24e631e04bb0cded691f2150
Tomas <tomas.oros0155@gmail.com>
2025-10-10 10:18:03 +0200

    updates - dark mode, UI enhacements

--- frontend/src/app/layout/sidebar/sidebar.component.ts
+++ frontend/src/app/layout/sidebar/sidebar.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [MatSidenavModule, MatListModule],
+  imports: [MatListModule, MatIconModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
