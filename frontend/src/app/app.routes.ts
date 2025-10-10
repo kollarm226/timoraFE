25a471127affc50f7b8c73b6df70f893595f0841
Daniel Dzvoník <daniel.dzvonik@kosickaakademia.sk>
2025-10-10 13:07:49 +0200

    Delete frontend directory

--- frontend/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
-
-export const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' }, // root presmeruje na login
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
-];
--- frontend/src/app/models/user.model.ts
+++
@@ -1,9 +0,0 @@
-// jednoduché rozhranie (interface) ktoré definuje čo obsahuje "User"
-export interface User {
-companyId: string; // ID firmy (unikátne)
-firstName: string; // krstné meno
-lastName: string; // priezvisko
-email: string; // e-mail (unikátny)
-address: string; // adresa
-password: string; // heslo (v reálnom projekte vždy hashované na serveri)
-}
