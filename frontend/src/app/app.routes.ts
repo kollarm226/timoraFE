cdca1eadf8f89e76e013426a5d1b80f2db99a02f
Tomas <tomas.oros0155@gmail.com>
2025-10-10 10:42:46 +0200

    fixed errors, lint problems

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
-  { path: '', component: DashboardComponent },
-  { path: '**', redirectTo: '' }
-import { RegisterComponent } from './register/register.component';
-
-export const routes: Routes = [
-  { path: '', redirectTo: '/register', pathMatch: 'full' }, // root presmeruje na register
-  { path: 'register', component: RegisterComponent }
+  { path: '', redirectTo: '/register', pathMatch: 'full' },
+  { path: 'register', component: RegisterComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '**', redirectTo: '/register' }
 ];
