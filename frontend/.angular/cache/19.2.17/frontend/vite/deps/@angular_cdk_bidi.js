e43b2c7950f16b2417968f3d8f28f8fdf720a3f5
Daniel Dzvonik <daniel.dzvonik@kosickaakademia.sk>
2025-10-14 10:10:14 +0200

    resolve mege conflicts

---
+++ frontend/.angular/cache/19.2.17/frontend/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,17 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-NG5TJCSS.js";
+import "./chunk-WFU6A2HP.js";
+import "./chunk-33T6IU7O.js";
+import "./chunk-6Q4RANH6.js";
+import "./chunk-FFZIAYYX.js";
+import "./chunk-CXCX2JKZ.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
