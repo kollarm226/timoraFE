0aad82ab05858b364dae9f7cd57f18caf82f2543
Daniel Dzvonik <daniel.dzvonik@kosickaakademia.sk>
2025-12-16 10:55:37 +0100

    Created documents page + added route to "request vacation" button

--- frontend/src/layout/topbar/topbar.component.ts
+++ frontend/src/layout/topbar/topbar.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 /**
  * Topbar komponent - horny navigacny panel
@@ -10,7 +11,7 @@ import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-topbar',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink],
   templateUrl: './topbar.component.html',
   styleUrl: './topbar.component.css'
 })
